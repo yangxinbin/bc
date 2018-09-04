1b85f4a84fa581e154e3a4bb205096e4a6acaec7
admin <yangxinbin@hotmail.com>
2018-09-04 12:25:06 +0800

    vip购买更新。

--- app/src/main/java/com/mango/bc/homepage/activity/OpenUpVipActivity.java
+++ app/src/main/java/com/mango/bc/homepage/activity/OpenUpVipActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.mango.bc.R;
 import com.mango.bc.base.BaseActivity;
@@ -13,30 +15,33 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class VipDetailActivity extends BaseActivity {
+public class OpenUpVipActivity extends BaseActivity {
 
     @Bind(R.id.imageView_back)
     ImageView imageViewBack;
-    @Bind(R.id.open_up)
-    Button openUp;
+    @Bind(R.id.list_item)
+    ListView listItem;
+    @Bind(R.id.tv_buy_detail)
+    TextView tvBuyDetail;
+    @Bind(R.id.buy_vip)
+    Button buyVip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vip_detail);
+        setContentView(R.layout.activity_open_up_vip);
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.imageView_back, R.id.open_up})
+    @OnClick({R.id.imageView_back, R.id.buy_vip})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.imageView_back:
-                finish();
-                break;
-            case R.id.open_up:
-                Intent intent = new Intent(this,OpenUpVipActivity.class);
+                Intent intent = new Intent(this,VipDetailActivity.class);
                 startActivity(intent);
                 finish();
+                break;
+            case R.id.buy_vip:
                 break;
         }
     }

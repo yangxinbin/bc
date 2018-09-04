ab6b0f1f788949f27412caaf83f32cddcee26f5c
admin <yangxinbin@hotmail.com>
2018-09-04 10:56:07 +0800

    vip介绍。

---
+++ app/src/main/java/com/mango/bc/homepage/activity/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.mango.bc.homepage.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mango.bc.R;
+import com.mango.bc.base.BaseActivity;
+
+public class SearchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
--- app/src/main/java/com/mango/bc/homepage/fragment/HeadFragment.java
+++ app/src/main/java/com/mango/bc/homepage/fragment/HeadFragment.java
@@ -1,19 +1,65 @@
 package com.mango.bc.homepage.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.mango.bc.R;
+import com.mango.bc.homepage.activity.CollageActivity;
+import com.mango.bc.homepage.activity.SearchActivity;
+import com.mango.bc.homepage.activity.VipDetailActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HeadFragment extends Fragment {
+    @Bind(R.id.imageView_vip)
+    ImageView imageViewVip;
+    @Bind(R.id.et_search)
+    EditText etSearch;
+    @Bind(R.id.imageView_collage)
+    ImageView imageViewCollage;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home, container, false);
+        ButterKnife.bind(this, view);
+        etSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.imageView_vip, R.id.imageView_collage})
+    public void onViewClicked(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.imageView_vip:
+                intent = new Intent(getContext(), VipDetailActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.imageView_collage:
+                intent = new Intent(getContext(), CollageActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }

ad187d4f0116163b25479255d9f0b7aff7af5869
admin <yangxinbin@hotmail.com>
2018-09-06 15:01:12 +0800

    拼团UI提交

---
+++ app/src/main/java/com/mango/bc/homepage/collage/fragment/CollageSuccessFragment.java
@@ -0,0 +1,23 @@
+package com.mango.bc.homepage.collage.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mango.bc.R;
+
+/**
+ * Created by admin on 2018/9/6.
+ */
+
+public class CollageSuccessFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.collage_success, container, false);
+        return view;
+    }
+}

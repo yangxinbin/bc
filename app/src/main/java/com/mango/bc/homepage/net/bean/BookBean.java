c147620eaa917403e22b76c0b0145b2c75e53eef
admin <yangxinbin@hotmail.com>
2018-09-12 12:13:30 +0800

    jsonBean优化。

--- app/src/main/java/com/mango/bc/homepage/net/bean/BookBean.java
+++ app/src/main/java/com/mango/bc/homepage/net/bean/BookBean.java
@@ -9,28 +9,28 @@ import java.util.List;
 public class BookBean {
 
     /**
-     * id : 5b84b31c04440c128c659ff2
-     * title : 贝尔福特：华尔街之狼
-     * subtitle : 出狱后比入狱前更赚钱的传奇人生。
-     * recommendation : 出狱后比入狱前更赚钱的传奇人生。
-     * descriptionImages : [{"id":"5b84b35304440c128c659ff3","alias":"郭宏才详情.jpg","fileName":"15354233153218227.jpg","contentType":"image/jpeg","size":49510,"createdOn":1535423315321}]
-     * cover : {"id":"5b87560e04440c002066b616","alias":"贝尔福特.jpg","fileName":"15355960465391611.jpg","contentType":"image/jpeg","size":97859,"createdOn":1535596046539}
-     * banner : null
-     * category : 人物系列
-     * author : {"id":"5b839f2b04440c1224e72ba7","name":"BC大陆","introduction":"no","photo":{"id":"5b8501b204440c128c65a11e","alias":"作者-BC大陆.jpg","fileName":"15354433784241925.jpg","contentType":"image/jpeg","size":39399,"createdOn":1535443378424}}
-     * chapters : [{"id":"1535423361229","title":"贝尔福特：华尔街之狼","duration":653,"audio":{"id":"5b84b3c004440c128c659ffa","alias":"贝尔福特完整版.mp3","fileName":"15354234248283889.mp3","contentType":"audio/mp3","size":15685975,"createdOn":1535423424828},"contentImages":[{"id":"5b84b39104440c128c659ff7","alias":"贝尔.jpg","fileName":"15354233774864373.jpg","contentType":"image/jpeg","size":512384,"createdOn":1535423377486}],"free":true,"index":1,"updatedOn":1535423361229}]
-     * tags : ["人物系列"]
-     * price : 0
-     * vipPrice : 0
-     * groupBuy2Price : 0
-     * groupBuy3Price : 0
-     * type : free
-     * likes : 13
+     * id : 5b8cb5ce04440c13249a643e
+     * title : 一起拆解区块链
+     * subtitle : 步入区块链世界的必修课，丰富人脉圈谈资的百宝箱。
+     * recommendation : 区块链被炒得这么火，到底什么才是区块链？！廉华老师带您揭开区块链的神秘面纱，一起感受新兴科技的魅力！
+     * descriptionImages : [{"id":"5b961c9804440c02c4555296","alias":"微信图片_20180910145903.jpg","fileName":"15365643760001476.jpg","contentType":"image/jpeg","size":914442,"createdOn":1536564376000}]
+     * cover : {"id":"5b8cb5ce04440c13249a643c","alias":"廉华400x300.jpg","fileName":"15359482386316416.jpg","contentType":"image/jpeg","size":69249,"createdOn":1535948238631}
+     * banner : {"id":"5b8cb5d404440c13249a643f","alias":"导师介绍-廉华-banner.jpg","fileName":"15359482441393248.jpg","contentType":"image/jpeg","size":37857,"createdOn":1535948244139}
+     * category : Other
+     * author : {"id":"5b85035804440c128c65a12f","name":"廉华","introduction":"","photo":{"id":"5b85035704440c128c65a12d","alias":"导师介绍-廉华-封面.jpg","fileName":"15354437990167324.jpg","contentType":"image/jpeg","size":34927,"createdOn":1535443799016}}
+     * chapters : [{"id":"1535955171006","title":"发刊词","duration":247,"audio":{"id":"5b921c5604440c02c4554cc9","alias":"发刊词.mp3","fileName":"15363021667298718.mp3","contentType":"audio/mp3","size":6080608,"createdOn":1536302166729},"contentImages":[{"id":"5b8cd0e804440c13249a645f","alias":"发刊词1.jpg","fileName":"15359551763619645.jpg","contentType":"image/jpeg","size":947843,"createdOn":1535955176361}],"free":true,"index":1,"updatedOn":1535955171006},{"id":"1535955297322","title":"区块链\u2014站在巨人肩膀的宠儿","duration":598,"audio":{"id":"5b8cd19104440c13249a648e","alias":"区块链\u2014站在巨人肩膀的宠儿.mp3","fileName":"15359553450021426.mp3","contentType":"audio/mpeg","size":14383627,"createdOn":1535955345002},"contentImages":[{"id":"5b8e542204440c12508dd7f0","alias":"廉华第一篇文章.jpg","fileName":"15360543065099194.jpg","contentType":"image/jpeg","size":817002,"createdOn":1536054306509}],"free":false,"index":2,"updatedOn":1535955297322},{"id":"1536300295001","title":"世界范围内的区块链发展现","duration":519,"audio":{"id":"5b92156204440c02c4554c88","alias":"世界范围内的区块链发展现状.mp3","fileName":"15363003869837312.mp3","contentType":"audio/mpeg","size":12767949,"createdOn":1536300386983},"contentImages":[{"id":"5b92156904440c02c4554cba","alias":"第2节：世界范围内的区块链发展现状.jpg","fileName":"15363003936907178.jpg","contentType":"image/jpeg","size":992837,"createdOn":1536300393690}],"free":false,"index":3,"updatedOn":1536300295001},{"id":"1536306856803","title":"国内外大企业区块链战略","duration":607,"audio":{"id":"5b922ec204440c02c4554d4c","alias":"国内外大企业区块链战略.mp3","fileName":"15363068823144351.mp3","contentType":"audio/mpeg","size":14933174,"createdOn":1536306882314},"contentImages":[{"id":"5b922eb104440c02c4554d47","alias":"第3节：国内外大企业区块链战略.jpg","fileName":"15363068655772693.jpg","contentType":"image/jpeg","size":906817,"createdOn":1536306865577}],"free":false,"index":4,"updatedOn":1536306856803}]
+     * tags : []
+     * price : 99.1
+     * vipPrice : 89.1
+     * groupBuy2Price : 79.2
+     * groupBuy3Price : 69.3
+     * type : paid
+     * likes : 12
      * likesPlus : 0
-     * sold : 70
+     * sold : 8
      * soldPlus : 0
-     * recommended : 102
-     * updatedOn : 1536139478337
+     * recommended : 300
+     * updatedOn : 1536564386695
      * updatedBy : 5b84ad3904440c128c32992b
      * status : online
      */
@@ -40,13 +40,13 @@ public class BookBean {
     private String subtitle;
     private String recommendation;
     private CoverBean cover;
-    private Object banner;
+    private BannerBean banner;
     private String category;
     private AuthorBean author;
-    private Double price;
-    private Double vipPrice;
-    private Double groupBuy2Price;
-    private Double groupBuy3Price;
+    private double price;
+    private double vipPrice;
+    private double groupBuy2Price;
+    private double groupBuy3Price;
     private String type;
     private int likes;
     private int likesPlus;
@@ -58,7 +58,7 @@ public class BookBean {
     private String status;
     private List<DescriptionImagesBean> descriptionImages;
     private List<ChaptersBean> chapters;
-    private List<String> tags;
+    private List<?> tags;
 
     public String getId() {
         return id;
@@ -100,11 +100,11 @@ public class BookBean {
         this.cover = cover;
     }
 
-    public Object getBanner() {
+    public BannerBean getBanner() {
         return banner;
     }
 
-    public void setBanner(Object banner) {
+    public void setBanner(BannerBean banner) {
         this.banner = banner;
     }
 
@@ -124,35 +124,35 @@ public class BookBean {
         this.author = author;
     }
 
-    public Double getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(Double price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    public Double getVipPrice() {
+    public double getVipPrice() {
         return vipPrice;
     }
 
-    public void setVipPrice(Double vipPrice) {
+    public void setVipPrice(double vipPrice) {
         this.vipPrice = vipPrice;
     }
 
-    public Double getGroupBuy2Price() {
+    public double getGroupBuy2Price() {
         return groupBuy2Price;
     }
 
-    public void setGroupBuy2Price(Double groupBuy2Price) {
+    public void setGroupBuy2Price(double groupBuy2Price) {
         this.groupBuy2Price = groupBuy2Price;
     }
 
-    public Double getGroupBuy3Price() {
+    public double getGroupBuy3Price() {
         return groupBuy3Price;
     }
 
-    public void setGroupBuy3Price(Double groupBuy3Price) {
+    public void setGroupBuy3Price(double groupBuy3Price) {
         this.groupBuy3Price = groupBuy3Price;
     }
 
@@ -244,22 +244,88 @@ public class BookBean {
         this.chapters = chapters;
     }
 
-    public List<String> getTags() {
+    public List<?> getTags() {
         return tags;
     }
 
-    public void setTags(List<String> tags) {
+    public void setTags(List<?> tags) {
         this.tags = tags;
     }
 
     public static class CoverBean {
         /**
-         * id : 5b87560e04440c002066b616
-         * alias : 贝尔福特.jpg
-         * fileName : 15355960465391611.jpg
+         * id : 5b8cb5ce04440c13249a643c
+         * alias : 廉华400x300.jpg
+         * fileName : 15359482386316416.jpg
          * contentType : image/jpeg
-         * size : 97859
-         * createdOn : 1535596046539
+         * size : 69249
+         * createdOn : 1535948238631
+         */
+
+        private String id;
+        private String alias;
+        private String fileName;
+        private String contentType;
+        private int size;
+        private long createdOn;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getAlias() {
+            return alias;
+        }
+
+        public void setAlias(String alias) {
+            this.alias = alias;
+        }
+
+        public String getFileName() {
+            return fileName;
+        }
+
+        public void setFileName(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public String getContentType() {
+            return contentType;
+        }
+
+        public void setContentType(String contentType) {
+            this.contentType = contentType;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+
+        public long getCreatedOn() {
+            return createdOn;
+        }
+
+        public void setCreatedOn(long createdOn) {
+            this.createdOn = createdOn;
+        }
+    }
+
+    public static class BannerBean {
+        /**
+         * id : 5b8cb5d404440c13249a643f
+         * alias : 导师介绍-廉华-banner.jpg
+         * fileName : 15359482441393248.jpg
+         * contentType : image/jpeg
+         * size : 37857
+         * createdOn : 1535948244139
          */
 
         private String id;
@@ -320,10 +386,10 @@ public class BookBean {
 
     public static class AuthorBean {
         /**
-         * id : 5b839f2b04440c1224e72ba7
-         * name : BC大陆
-         * introduction : no
-         * photo : {"id":"5b8501b204440c128c65a11e","alias":"作者-BC大陆.jpg","fileName":"15354433784241925.jpg","contentType":"image/jpeg","size":39399,"createdOn":1535443378424}
+         * id : 5b85035804440c128c65a12f
+         * name : 廉华
+         * introduction :
+         * photo : {"id":"5b85035704440c128c65a12d","alias":"导师介绍-廉华-封面.jpg","fileName":"15354437990167324.jpg","contentType":"image/jpeg","size":34927,"createdOn":1535443799016}
          */
 
         private String id;
@@ -365,12 +431,12 @@ public class BookBean {
 
         public static class PhotoBean {
             /**
-             * id : 5b8501b204440c128c65a11e
-             * alias : 作者-BC大陆.jpg
-             * fileName : 15354433784241925.jpg
+             * id : 5b85035704440c128c65a12d
+             * alias : 导师介绍-廉华-封面.jpg
+             * fileName : 15354437990167324.jpg
              * contentType : image/jpeg
-             * size : 39399
-             * createdOn : 1535443378424
+             * size : 34927
+             * createdOn : 1535443799016
              */
 
             private String id;
@@ -432,12 +498,12 @@ public class BookBean {
 
     public static class DescriptionImagesBean {
         /**
-         * id : 5b84b35304440c128c659ff3
-         * alias : 郭宏才详情.jpg
-         * fileName : 15354233153218227.jpg
+         * id : 5b961c9804440c02c4555296
+         * alias : 微信图片_20180910145903.jpg
+         * fileName : 15365643760001476.jpg
          * contentType : image/jpeg
-         * size : 49510
-         * createdOn : 1535423315321
+         * size : 914442
+         * createdOn : 1536564376000
          */
 
         private String id;
@@ -498,14 +564,14 @@ public class BookBean {
 
     public static class ChaptersBean {
         /**
-         * id : 1535423361229
-         * title : 贝尔福特：华尔街之狼
-         * duration : 653
-         * audio : {"id":"5b84b3c004440c128c659ffa","alias":"贝尔福特完整版.mp3","fileName":"15354234248283889.mp3","contentType":"audio/mp3","size":15685975,"createdOn":1535423424828}
-         * contentImages : [{"id":"5b84b39104440c128c659ff7","alias":"贝尔.jpg","fileName":"15354233774864373.jpg","contentType":"image/jpeg","size":512384,"createdOn":1535423377486}]
+         * id : 1535955171006
+         * title : 发刊词
+         * duration : 247
+         * audio : {"id":"5b921c5604440c02c4554cc9","alias":"发刊词.mp3","fileName":"15363021667298718.mp3","contentType":"audio/mp3","size":6080608,"createdOn":1536302166729}
+         * contentImages : [{"id":"5b8cd0e804440c13249a645f","alias":"发刊词1.jpg","fileName":"15359551763619645.jpg","contentType":"image/jpeg","size":947843,"createdOn":1535955176361}]
          * free : true
          * index : 1
-         * updatedOn : 1535423361229
+         * updatedOn : 1535955171006
          */
 
         private String id;
@@ -583,12 +649,12 @@ public class BookBean {
 
         public static class AudioBean {
             /**
-             * id : 5b84b3c004440c128c659ffa
-             * alias : 贝尔福特完整版.mp3
-             * fileName : 15354234248283889.mp3
+             * id : 5b921c5604440c02c4554cc9
+             * alias : 发刊词.mp3
+             * fileName : 15363021667298718.mp3
              * contentType : audio/mp3
-             * size : 15685975
-             * createdOn : 1535423424828
+             * size : 6080608
+             * createdOn : 1536302166729
              */
 
             private String id;
@@ -649,12 +715,12 @@ public class BookBean {
 
         public static class ContentImagesBean {
             /**
-             * id : 5b84b39104440c128c659ff7
-             * alias : 贝尔.jpg
-             * fileName : 15354233774864373.jpg
+             * id : 5b8cd0e804440c13249a645f
+             * alias : 发刊词1.jpg
+             * fileName : 15359551763619645.jpg
              * contentType : image/jpeg
-             * size : 512384
-             * createdOn : 1535423377486
+             * size : 947843
+             * createdOn : 1535955176361
              */
 
             private String id;

cffe3c028cf5d60946001a89957d6e938c92f3d8
admin <yangxinbin@hotmail.com>
2018-09-20 14:24:49 +0800

    交易记录UI

--- app/src/main/java/com/mango/bc/wallet/adapter/TransactionAdapter.java
+++ app/src/main/java/com/mango/bc/wallet/adapter/TransactionAdapter.java
@@ -84,6 +84,12 @@ public class TransactionAdapter extends RecyclerView.Adapter {
                     viewHolder.tv_time.setText(DateUtil.getDateToString(datas.get(position).getTimestamp(),"yyyy-MM-dd HH:mm:ss"));
                     viewHolder.tv_pp.setText("+"+datas.get(position).getPpCoin()+"积分");
                     viewHolder.tv_pp.setTextColor(context.getResources().getColor(R.color.red));
+                }else if (datas.get(position).getType().equals("transfer")){
+                    viewHolder.img_transaction.setImageResource(R.drawable.record_transfer);
+                    viewHolder.tv_title.setText("转账给"+datas.get(position).getMeta());
+                    viewHolder.tv_time.setText(DateUtil.getDateToString(datas.get(position).getTimestamp(),"yyyy-MM-dd HH:mm:ss"));
+                    viewHolder.tv_pp.setText(datas.get(position).getPpCoin()+"积分");
+                    viewHolder.tv_pp.setTextColor(context.getResources().getColor(R.color.common_black));
                 }
             }
         }

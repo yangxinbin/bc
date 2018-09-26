a219c5558dd530a76cdc508d127743959c708c4d
admin <yangxinbin@hotmail.com>
2018-09-26 17:03:56 +0800

    首页播放全局控件

--- app/src/main/java/com/mango/bc/homepage/HomePageFragment.java
+++ app/src/main/java/com/mango/bc/homepage/HomePageFragment.java
@@ -1,29 +1,35 @@
 package com.mango.bc.homepage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-import com.google.gson.Gson;
 import com.mango.bc.R;
-import com.mango.bc.bookcase.net.bean.MyBookBean;
 import com.mango.bc.bookcase.net.presenter.MyBookPresenter;
 import com.mango.bc.bookcase.net.presenter.MyBookPresenterImpl;
 import com.mango.bc.bookcase.net.view.MyAllBookView;
 import com.mango.bc.homepage.adapter.HomePageAdapter;
+import com.mango.bc.homepage.bookdetail.bean.PlayBarBean;
+import com.mango.bc.homepage.bookdetail.play.BaseServiceFragment;
+import com.mango.bc.homepage.bookdetail.play.PlayFragment;
+import com.mango.bc.homepage.bookdetail.play.constants.Extras;
+import com.mango.bc.homepage.bookdetail.play.executor.ControlPanel;
+import com.mango.bc.homepage.bookdetail.play.service.AudioPlayer;
 import com.mango.bc.homepage.net.bean.LoadStageBean;
 import com.mango.bc.homepage.net.bean.RefreshStageBean;
 import com.mango.bc.homepage.net.presenter.BookPresenter;
 import com.mango.bc.util.AppUtils;
 import com.mango.bc.util.NetUtil;
-import com.mango.bc.util.SPUtils;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.header.ClassicsHeader;
@@ -31,9 +37,8 @@ import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import org.greenrobot.eventbus.EventBus;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -42,17 +47,22 @@ import butterknife.ButterKnife;
  * Created by admin on 2018/9/3.
  */
 
-public class HomePageFragment extends Fragment implements MyAllBookView {
+public class HomePageFragment extends BaseServiceFragment implements MyAllBookView {
     @Bind(R.id.recycle)
     RecyclerView recycle;
     @Bind(R.id.refresh)
     SmartRefreshLayout refresh;
+    @Bind(R.id.fl_play_bar)
+    FrameLayout flPlayBar;
     private boolean isFirstEnter = true;
     private HomePageAdapter homePageAdapter;
     private BookPresenter bookPresenter;
     private final int TYPE = 3;
     private int page = 0;
     private MyBookPresenter myBookPresenter;
+    private ControlPanel controlPanel;
+    private boolean isPlayFragmentShow;
+    private PlayFragment mPlayFragment;
 
     @Nullable
     @Override
@@ -60,12 +70,64 @@ public class HomePageFragment extends Fragment implements MyAllBookView {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.homepage, container, false);
         myBookPresenter = new MyBookPresenterImpl(this);
         ButterKnife.bind(this, view);
+        EventBus.getDefault().register(this);
         initView();
         refreshAndLoadMore();
         return view;
     }
+    @Override
+    protected void onServiceBound() {
+        controlPanel = new ControlPanel(flPlayBar);
+        AudioPlayer.get().addOnPlayEventListener(controlPanel);
+        parseIntent();
+    }
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    public void PlayBarBeanEventBus(PlayBarBean playBarBean) {
+        if (playBarBean == null) {
+            return;
+        }
+        Log.v("iiiiiiiiiiiiii","---iiiiiiiiiiii---");
+        if (!playBarBean.isShowBar()) {
+            flPlayBar.setVisibility(View.GONE);//播放控件
+            Log.v("iiiiiiiiiiiiii","----h---");
+        }
+        EventBus.getDefault().removeStickyEvent(PlayBarBean.class);
+    }
+    private void parseIntent() {
+        Intent intent = getActivity().getIntent();
+        if (intent.hasExtra(Extras.EXTRA_NOTIFICATION)) {
+            showPlayingFragment();
+            getActivity().setIntent(new Intent());
+        }
+    }
 
+    private void showPlayingFragment() {
+        if (isPlayFragmentShow) {
+            return;
+        }
+
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.setCustomAnimations(R.anim.fragment_slide_up, 0);
+        if (mPlayFragment == null) {
+            mPlayFragment = new PlayFragment();
+            ft.replace(android.R.id.content, mPlayFragment);
+        } else {
+            ft.show(mPlayFragment);
+        }
+        ft.commitAllowingStateLoss();
+        isPlayFragmentShow = true;
+    }
+
+    private void hidePlayingFragment() {
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.setCustomAnimations(0, R.anim.fragment_slide_down);
+        ft.hide(mPlayFragment);
+        ft.commitAllowingStateLoss();
+        isPlayFragmentShow = false;
+    }
     private void initView() {
+        if (AudioPlayer.get().isPlaying() || AudioPlayer.get().isPausing())
+            flPlayBar.setVisibility(View.VISIBLE);//播放控件
         homePageAdapter = new HomePageAdapter(getActivity());
         recycle.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
         recycle.setAdapter(homePageAdapter);

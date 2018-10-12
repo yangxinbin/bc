1a44dd7f3e42e281a9d6dcbaeb3626640bd2c64d
admin <yangxinbin@hotmail.com>
2018-10-12 16:35:43 +0800

    UI与接口：我的成员

--- app/src/main/java/com/mango/bc/mine/activity/MemberActivity.java
+++ app/src/main/java/com/mango/bc/mine/activity/MemberActivity.java
@@ -1,16 +1,61 @@
 package com.mango.bc.mine.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.mango.bc.R;
 import com.mango.bc.base.BaseActivity;
+import com.mango.bc.login.adapter.DuoXuanAdapter;
+import com.mango.bc.login.adapter.NoScrollGridView;
+import com.mango.bc.mine.adapter.MemberAdapter;
+import com.mango.bc.mine.bean.MemberBean;
+import com.mango.bc.mine.jsonutil.MineJsonUtils;
+import com.mango.bc.util.SPUtils;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MemberActivity extends BaseActivity {
+
+    @Bind(R.id.tv_num)
+    TextView tvNum;
+    @Bind(R.id.tv_ppg)
+    TextView tvPpg;
+    @Bind(R.id.gv)
+    NoScrollGridView gv;
+    private SPUtils spUtils;
+    private MemberAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member);
+        spUtils = SPUtils.getInstance("bc", this);
+        ButterKnife.bind(this);
+        initMember(MineJsonUtils.readMemberBean(spUtils.getString("member", "")));
+    }
+
+    private void initMember(MemberBean member) {
+        if (member == null)
+            return;
+        tvNum.setText("已有成员"+member.getSize()+"人");
+        tvPpg.setText("累计收益"+member.getTotal()+"积分");
+        initView(member.getUsers());
+    }
+
+    private void initView(List<MemberBean.UsersBean> list) {
+        adapter = new MemberAdapter(this, list);
+        gv.setAdapter(adapter);
+    }
+
+    @OnClick(R.id.imageView_back)
+    public void onViewClicked() {
+        Intent intent = new Intent(this, MemberActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/mango/bc/mine/adapter/MemberAdapter.java
@@ -0,0 +1,87 @@
+package com.mango.bc.mine.adapter;
+
+/**
+ * Created by admin on 2018/5/29.
+ */
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.mango.bc.R;
+import com.mango.bc.mine.activity.MemberActivity;
+import com.mango.bc.mine.bean.MemberBean;
+import com.mango.bc.util.Urls;
+
+import java.util.List;
+import java.util.Map;
+
+
+public class MemberAdapter extends BaseAdapter {
+
+    private List<MemberBean.UsersBean> beans;
+    private Context mContext;
+    private boolean isFlag = true;
+
+    public MemberAdapter(MemberActivity mContext, List<MemberBean.UsersBean> list) {
+        this.mContext = mContext;
+        this.beans = list;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return beans.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+
+    @Override
+    public View getView(final int position, View containView, final ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (containView != null) {
+            viewHolder = (ViewHolder) containView.getTag();
+        } else {
+            containView = LayoutInflater.from(mContext).inflate(R.layout.member_default_down, null);
+            viewHolder = new ViewHolder();
+            viewHolder.tv_name = containView.findViewById(R.id.tv_name);
+            viewHolder.tv_get = containView.findViewById(R.id.tv_get);
+            viewHolder.imageVie_pic = containView.findViewById(R.id.imageVie_pic);
+            viewHolder.co = containView.findViewById(R.id.con);
+            containView.setTag(viewHolder);
+        }
+        if (beans.get(position).getAvator() != null) {
+            Glide.with(mContext).load(Urls.HOST_GETFILE + "?name=" + beans.get(position).getAvator().getFileName()).into(viewHolder.imageVie_pic);
+        } else {
+            viewHolder.imageVie_pic.setImageDrawable(mContext.getResources().getDrawable(R.drawable.head_pic2));
+        }
+        viewHolder.tv_name.setText(beans.get(position).getAlias());
+        viewHolder.tv_get.setText(beans.get(position).getWalletAddress());
+        return containView;
+    }
+}
+
+class ViewHolder {
+    TextView tv_name, tv_get;
+    LinearLayout co;
+    ImageView imageVie_pic;
+}
+

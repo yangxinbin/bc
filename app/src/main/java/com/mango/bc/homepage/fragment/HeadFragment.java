63bd8430d62d9105bc9c9b447905e24f6d53e7f1
admin <yangxinbin@hotmail.com>
2018-10-17 14:37:57 +0800

    UI 轮播圆角

--- app/src/main/java/com/mango/bc/homepage/fragment/HeadFragment.java
+++ app/src/main/java/com/mango/bc/homepage/fragment/HeadFragment.java
@@ -19,6 +19,7 @@ import com.mango.bc.homepage.activity.VipDetailActivity;
 import com.mango.bc.util.GlideImageLoader;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
 import com.youth.banner.listener.OnBannerClickListener;
 
 import java.util.ArrayList;
@@ -31,33 +32,33 @@ import butterknife.OnClick;
 public class HeadFragment extends Fragment {
     @Bind(R.id.et_search)
     EditText etSearch;
- /*   @Bind(R.id.imageView_collage)
-    ImageView imageViewCollage;
-    @Bind(R.id.imageView_novip)
-    ImageView imageViewNovip;
-    @Bind(R.id.imageVie_author)
-    CircleImageView imageVieAuthor;
-    @Bind(R.id.tv_vipName)
-    TextView tvVipName;
-    @Bind(R.id.img_vip)
-    ImageView imgVip;
-    @Bind(R.id.img_agency)
-    ImageView imgAgency;
-    @Bind(R.id.tv_vipTime)
-    TextView tvVipTime;
-    @Bind(R.id.buy_vip)
-    TextView buyVip;
-    @Bind(R.id.tv_bookReadDay)
-    TextView tvBookReadDay;
-    @Bind(R.id.tv_vipTip)
-    TextView tvVipTip;
-    @Bind(R.id.tv_vipContent1)
-    TextView tvVipContent1;
-    @Bind(R.id.tv_vipContent2)
-    TextView tvVipContent2;
-    @Bind(R.id.l_vip)
-    LinearLayout lVip;
-    private SPUtils spUtils;*/
+    /*   @Bind(R.id.imageView_collage)
+       ImageView imageViewCollage;
+       @Bind(R.id.imageView_novip)
+       ImageView imageViewNovip;
+       @Bind(R.id.imageVie_author)
+       CircleImageView imageVieAuthor;
+       @Bind(R.id.tv_vipName)
+       TextView tvVipName;
+       @Bind(R.id.img_vip)
+       ImageView imgVip;
+       @Bind(R.id.img_agency)
+       ImageView imgAgency;
+       @Bind(R.id.tv_vipTime)
+       TextView tvVipTime;
+       @Bind(R.id.buy_vip)
+       TextView buyVip;
+       @Bind(R.id.tv_bookReadDay)
+       TextView tvBookReadDay;
+       @Bind(R.id.tv_vipTip)
+       TextView tvVipTip;
+       @Bind(R.id.tv_vipContent1)
+       TextView tvVipContent1;
+       @Bind(R.id.tv_vipContent2)
+       TextView tvVipContent2;
+       @Bind(R.id.l_vip)
+       LinearLayout lVip;
+       private SPUtils spUtils;*/
     @Bind(R.id.imageView)
     Banner imageView;
 
@@ -82,32 +83,32 @@ public class HeadFragment extends Fragment {
         return view;
     }
 
-/*    private void initView(UserBean auth) {
-        if (auth == null)
-            return;
-        if (auth.isVip()) {
-            lVip.setVisibility(View.VISIBLE);
-            imgVip.setVisibility(View.VISIBLE);
-            imageViewNovip.setVisibility(View.GONE);
-        } else {
-            lVip.setVisibility(View.GONE);
-            imgVip.setVisibility(View.GONE);
-            imageViewNovip.setVisibility(View.VISIBLE);
+    /*    private void initView(UserBean auth) {
+            if (auth == null)
+                return;
+            if (auth.isVip()) {
+                lVip.setVisibility(View.VISIBLE);
+                imgVip.setVisibility(View.VISIBLE);
+                imageViewNovip.setVisibility(View.GONE);
+            } else {
+                lVip.setVisibility(View.GONE);
+                imgVip.setVisibility(View.GONE);
+                imageViewNovip.setVisibility(View.VISIBLE);
+            }
+            tvVipName.setText(auth.getAlias());
+            if (auth.getAvator() != null)
+                Glide.with(getActivity()).load(Urls.HOST_GETFILE + "?name=" + auth.getAvator().getFileName()).into(imageVieAuthor);
+            if (auth.getBilling() != null)
+                tvVipTime.setText("至" + DateUtil.getDateToString(auth.getBilling().getEndOn(), "yyyy-MM-dd"));
+
         }
-        tvVipName.setText(auth.getAlias());
-        if (auth.getAvator() != null)
-            Glide.with(getActivity()).load(Urls.HOST_GETFILE + "?name=" + auth.getAvator().getFileName()).into(imageVieAuthor);
-        if (auth.getBilling() != null)
-            tvVipTime.setText("至" + DateUtil.getDateToString(auth.getBilling().getEndOn(), "yyyy-MM-dd"));
 
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
-    public void UserBeanEventBus(UserBean userBean) {//用于实时更新
-        if (userBean == null)
-            return;
-        initView(userBean);
-    }*/
+        @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+        public void UserBeanEventBus(UserBean userBean) {//用于实时更新
+            if (userBean == null)
+                return;
+            initView(userBean);
+        }*/
     private void initBanner() {
         List<String> pathsImage = new ArrayList<>();
         List<String> pathsTitle = new ArrayList<>();
@@ -115,13 +116,14 @@ public class HeadFragment extends Fragment {
         pathsImage.add(getResourcesUri(R.drawable.banner));
         pathsTitle.add("");
         pathsTitle.add("");
-        imageView.setImages(pathsImage)
+        imageView.setImageLoader(new GlideImageLoader())
                 .setBannerTitles(pathsTitle)
+                .setImages(pathsImage)
                 .setBannerStyle(BannerConfig.CIRCLE_INDICATOR)
-                .setImageLoader(new GlideImageLoader())
                 .setDelayTime(5000)
                 // .setOnBannerClickListener(this)
-                .start();
+                .start()
+                .setBannerAnimation(Transformer.Stack);
         imageView.setOnBannerClickListener(new OnBannerClickListener() {
             @Override
             public void OnBannerClick(int position) {
